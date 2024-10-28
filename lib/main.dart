9db2d3011a96f106470b53d59bb0472a57e8bcd2
Krish-Kalyani <kkalyani1@student.gsu.edu>
2024-10-28 15:13:01 -0400

    User Data Inputs etc

--- lib/main.dart
+++ lib/main.dart
@@ -9,3 +9,29 @@ void main() {
     ),
   );
 }
+//USER DATA
+class UserData {
+  double income;
+  double houseExpense;
+  double groceriesExpense;
+  double entertainmentExpense;
+  double transportationExpense;
+  double miscExpense;
+  double emergencySavings;
+  double vacationSavings;
+  double medicalSavings;
+  double shoppingSavings;
+  double investments;
+  UserData({
+    this.income = 0.0,
+    this.houseExpense = 0.0,
+    this.groceriesExpense = 0.0,
+    this.entertainmentExpense = 0.0,
+    this.transportationExpense = 0.0,
+    this.miscExpense = 0.0,
+    this.emergencySavings = 0.0,
+    this.vacationSavings = 0.0,
+    this.medicalSavings = 0.0,
+    this.shoppingSavings = 0.0,
+    this.investments = 0.0,
+  });
